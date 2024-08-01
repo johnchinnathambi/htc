ec17f8d8fefca2d1cf2303a10a83d6863670c254
john <php.john.c@gmail.com>
2024-08-01 18:02:21 +0530

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+  // const date = new Date()
+  // const currentYear = date.getFullYear()
+
+  return (
+    <footer className='text-primary container-fluid'>
+      {/* <div className='row'>
+        <div className='col text-center py-1 footer font-monospace bg-light my-auto'>
+          { Copyright {currentYear} &copy; All Rights Reserved -  }
+          Developed by{' '}
+          <a target='_blank' href='http://aiotechsoft.com' rel='noreferrer'>
+            John Chinnathambi
+          </a>
+          <br />
+          <img src='/logo.png' width='30' height='30' alt='logo' />
+        </div>
+      </div> */}
+    </footer>
+  )
+}
+
+export default Footer
