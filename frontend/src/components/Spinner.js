89def0058d8a1650ffa09d5e4d69e91fcc86d59e
Velmurugan S <velmurugan.sivasubramanian@excelenciaconsulting.com>
2024-09-02 06:52:29 +0530

    loader fix

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -1,7 +1,7 @@
 export const Spinner = (props) => {
   const { height = "1", stroke = "bg-blue-500" } = props;
   return (
-    <div className="w-full top-0 left-0 z-[999]">
+    <div className="absolute w-full top-0 left-0 z-[999]">
       <div className={`h-${height} w-full bg-blue-100 overflow-hidden`}>
         <span className="sr-only">Loading...</span>
         <div
